adedc2bc45d913ebbbba91ab9c5c3087a52ef216
quaddss52 <tomilayojesse@gmail.com>
2025-08-08 13:48:56 +0100

    fixing css deployment

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx,mdx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
